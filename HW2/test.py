17aad6330f643f352dfc6dc905a00fee13a5374a
JCrossover-14 <ruijie.xiao@stonybrook.edu>
2026-03-22 19:44:39 -0400

    added generic tls server

--- HW2/test.py
+++ HW2/test.py
@@ -1,4 +1,7 @@
 import socket
+import ssl 
+'''
+TCP Section
 
 s = socket.socket()
 s.bind(('0.0.0.0', 3131))
@@ -21,3 +24,29 @@ while True:
         pass
     
     conn.close()
+'''
+
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+bind_sock = socket.socket()
+bind_sock.bind((HOST, PORT))
+bind_sock.listen(5)
+print(f"Generic TLS server listening on port {PORT}...")
+
+while True:
+    client_sock, addr = bind_sock.accept()
+    print(f"Connection from {addr}")
+
+    # Wrap socket in TLS
+    tls_conn = context.wrap_socket(client_sock, server_side=True)
+
+    # Wait for client data
+    data = tls_conn.recv(1024)
+
+    # Only respond if it's the generic probe
+    if data.strip() == b"\r\n\r\n\r\n\r\n":
+        tls_conn.sendall(b"generic TLS reply")
+
+    tls_conn.close()
