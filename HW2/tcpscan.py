d344a1ce31e5670d9a10d54d60b132eea71653b0
JCrossover-14 <ruijie.xiao@stonybrook.edu>
2026-03-23 01:31:27 -0400

    deleted debug prints and unnecessary code

--- HW2/tcpscan.py
+++ HW2/tcpscan.py
@@ -10,9 +10,9 @@ DEFAULT_PORTS = [21, 22, 23, 25, 80, 110, 143, 443, 587, 853, 993, 3389, 8080]
 
 
 def get_cn(cert):
-    print(f'cert is {cert}')
+    #print(f'cert is {cert}')
     try:
-        print(f'subjectAltName is {cert.get("subjectAltName")}')
+        #print(f'subjectAltName is {cert.get("subjectAltName")}')
         for typ, val in cert.get("subjectAltName", []):
             if typ == "DNS":
                 return val
@@ -21,7 +21,7 @@ def get_cn(cert):
 
     try:
         subj = cert.get("subject",[])
-        print(f'subj is {subj}')
+        #print(f'subj is {subj}')
         for item in subj:
             for key,value in item:
                 if key=='commonName':
@@ -155,13 +155,13 @@ else:
             ports.add(p) 
 
 
-print(f'target is {args.target}')
-print(f'ports are {ports}')
+#print(f'target is {args.target}')
+#print(f'ports are {ports}')
         
 open_ports = set()
 
 for port in ports:
-    print(f'Trying to scan port {port}')
+    #print(f'Trying to scan port {port}')
     pkt = IP(dst=args.target)/TCP(dport=port,flags="S")
     
     resp = sr1(pkt,timeout=1,verbose=0)
@@ -172,21 +172,22 @@ for port in ports:
         flags = resp[TCP].flags 
         # SYN + ACK = 0x02+0x10=0x12
         if flags==0x12:
-            print(f'port {port} is open')
+            #print(f'port {port} is open')
             open_ports.add(port)
 
             # send rst close connection
             rst_pkt = IP(dst=args.target)/TCP(dport=port,flags="R")
             send(rst_pkt,verbose=0)
         elif flags==0x14:
-            print(f'RST received, closed')
-print(f'open ports are {open_ports}')
+            pass
+            #print(f'RST received, closed')
+#print(f'open ports are {open_ports}')
 
 
-print("\n service fingerprinting now")
+#print("\n service fingerprinting now")
 
 for port in open_ports:
-    print(f'trying port {port}')
+    #print(f'trying port {port}')
 
     is_tls = False
     cn = "unknown"
@@ -219,125 +220,8 @@ for port in open_ports:
             print("Response: none\n")
         continue
 
-# otherwise TCP
+    # otherwise TCP
     probe_tcp(args.target, port)
     continue 
-    # TLS
-    cn = "unknown"
-
-    try:
-        context = ssl.create_default_context()
-        context.check_hostname = False
-        context.verify_mode = ssl.CERT_NONE
-
-        sock = socket.create_connection((args.target, port), timeout=2)
-        tls_sock = context.wrap_socket(sock, server_hostname=args.target)
-        tls_sock.settimeout(2)
-
-        cert = tls_sock.getpeercert()
-        cn = get_cn(cert)
-
-        # (2) TLS server-initiated
-        try:
-            data = tls_sock.recv(1024)
-            if data:
-                print(f"Host: {args.target}:{port}")
-                print(f"Type: (2) TLS server-initiated | CN {cn}")
-                print("Response: ...\n")
-                tls_sock.close()
-                continue
-        except:
-            pass
-
-        # (4) HTTPS
-        try:
-            tls_sock.sendall(b"GET / HTTP/1.0\r\n\r\n")
-            data = tls_sock.recv(1024)
-            if data:
-                print(f"Host: {args.target}:{port}")
-                print(f"Type: (4) HTTPS server | CN {cn}")
-                print("Response: ...\n")
-                tls_sock.close()
-                continue
-        except:
-            pass
-
-        # (6) Generic TLS
-        try:
-            tls_sock.sendall(b"\r\n\r\n\r\n\r\n")
-            data = tls_sock.recv(1024)
-        except (socket.timeout, ConnectionResetError):
-            data = b""
-
-        print(f"Host: {args.target}:{port}")
-        print(f"Type: (6) Generic TLS server | CN {cn}")
-        print("Response: none\n")
-
-        tls_sock.close()
-        continue
-    except Exception as e:
-        pass
     
-    # TCP
-    try:
-        s = socket.create_connection((args.target,port),timeout=2)
-        s.settimeout(2)
-        # TCP banner
-        try:
-            data=s.recv(1024)
-            if data:
-                data = ''.join(chr(b) for b in data)
-                print("Type: (1) TCP server-initialted")
-                print(f"Response: {data[:1024]}\n")
-
-                s.close()
-                continue 
-        except socket.timeout:
-            pass
-        # try http now 
-        try:
-            s.close() 
-            s = socket.create_connection((args.target,port),timeout=2)
-            s.settimeout(2)
-            s.sendall(b"GET / HTTP/1.0\r\n\r\n")
-            data = s.recv(1024)
-
-            if data:
-                data = ''.join(chr(b) for b in data)
-                print(f'Type: (3) HTTP server')
-                print(f"Response: {data[:1024]}\n")
-
-                s.close()
-                continue 
-        except socket.timeout:
-            pass
-        # generic tcp
-        try:
-            s.close()
-            s=socket.create_connection((args.target,port),timeout=2)
-            s.settimeout(2)
-
-            s.sendall(b"\r\n\r\n\r\n\r\n")
-            data=s.recv(1024)
-
-            print("Type: (5) Generic TCP server")
-            if data:
-                data=''.join(chr(b) for b in data)
-                print(f'Response: {data[:1024]}\n')
-            else:
-                print("Response: none\n")
-            continue 
-        except (socket.timeout,ConnectionResetError):
-            print(f"Type: (5) generic TCP server")
-            print("Response: none\n")
-            pass 
-        s.close()
-    except Exception as e:
-        print(f'something wrong happened error is {e}')
-        pass 
-    
-    
-        
-        
-        
     
