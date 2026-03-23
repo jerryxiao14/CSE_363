8ad1d0202fe5d69eaa7e378c39be19d571a548d1
JCrossover-14 <ruijie.xiao@stonybrook.edu>
2026-03-22 23:16:46 -0400

    hi

--- HW2/test.py
+++ HW2/test.py
@@ -1,4 +1,32 @@
-import ssl, socket
+import socket
+import ssl 
+'''
+TCP Section
+
+s = socket.socket()
+s.bind(('0.0.0.0', 3131))
+s.listen(1)
+
+print("Generic TCP server listening on port 3131...")
+
+while True:
+    conn, addr = s.accept()
+    print(f"Connection from {addr}")
+
+    # Wait for client data
+    data = conn.recv(1024)
+    
+    # Only respond if the data looks like a generic probe
+    if data == b"\r\n\r\n\r\n\r\n":
+        conn.sendall(b"generic reply")
+    else:
+        # Ignore everything else (HTTP GET, banner requests, etc.)
+        pass
+    
+    conn.close()
+'''
+
+
 
 MODE = 2   # change to 2, 4, or 6
 
